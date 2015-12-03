818519f4ac9c3ee1af0b141dc589a2b5b7bc158e
Matthammond96 <ultramatt96@hotmail.co.uk>
2015-12-03 16:39:39 +0000

    Initial Commit

---
+++ Relive/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Relive
+//
+//  Created by Matthew Hammond on 03/12/2015.
+//  Copyright © 2015 Matthew Hammond. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ ReliveTests/ReliveTests.swift
@@ -0,0 +1,36 @@
+//
+//  ReliveTests.swift
+//  ReliveTests
+//
+//  Created by Matthew Hammond on 03/12/2015.
+//  Copyright © 2015 Matthew Hammond. All rights reserved.
+//
+
+import XCTest
+@testable import Relive
+
+class ReliveTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
